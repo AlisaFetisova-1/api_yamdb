1e74d79cf48fb72126654380dc0e015d1642cb58
Владас Куодис <vlad250577@yandex.ru>
2023-03-17 22:49:11 +0300

    feature_users_2

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework import permissions
+
+
+class AdminOrSuperuser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return request.user._is_admin
+        return False
---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+
+from reviews.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'username', 'email', 'first_name',
+            'last_name', 'bio', 'role')
+        
+    def validate_username(self, username):
+        if username == 'me':
+            raise serializers.ValidationError('Вы не можете использовать "me"!')
+        return username
+
+
+class MeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'username', 'email', 'first_name',
+            'last_name', 'bio', 'role')
+        read_only_fields = ('role',)
+
+class GetTokenSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        required=True)
+    confirmation_code = serializers.CharField(
+        required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'confirmation_code'
+        )
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('email', 'username')
+
