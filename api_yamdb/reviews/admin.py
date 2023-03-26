db803486967bad30706156f4b5cf6b8c2656bb30
Алиса Фетисова <Fetisova.alisa2002@yandex.ru>
2023-03-26 15:20:08 +0300

    10

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-
-from .models import Comment, Review, Title, GenreTitle, Genre, Category 
+from .models import (Comment, Review,
+                     Title, GenreTitle,
+                     Genre, Category) 
 
 admin.site.register(Comment)
 admin.site.register(Review)
